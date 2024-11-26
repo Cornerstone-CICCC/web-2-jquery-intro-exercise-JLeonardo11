45825fd4b1a2789da5ca3d1e4428813ded08d9f9
Justin Leonardo <J.leonardo11n@gmail.com>
2024-11-26 08:55:11 -0800

    Done

--- app.js
+++ app.js
@@ -1,3 +1,36 @@
-$(function() {
-  // YOUR CODE HERE
-})
+$('#toggleButton').on('click', () => {
+  $('#myParagraph').toggle(); // Toggles between show and hide
+});
+
+$('#colorButton').on('click', () => {
+  $('#colorDiv').css({
+    'background-color':'cyan'
+  }); 
+});
+
+$('#addClassButton').on('click', () => {
+  
+  $('#classDiv').addClass('newClass');
+  
+
+});
+
+$('#removeClassButton').on('click', () => {
+  $('#classDiv').removeClass('newClass');
+});
+
+$('#fadeInButton').on('click', () => {
+  $('#fadeDiv').fadeIn();
+});
+
+
+$('#fadeOutButton').on('click', () => {
+  $('#fadeDiv').fadeOut();
+});
+
+$('#slideUpButton').on('click', () => {
+  $('#slideDiv').slideUp(); 
+});
+$('#slideDownButton').on('click', () => {
+  $('#slideDiv').slideDown(); 
+});
